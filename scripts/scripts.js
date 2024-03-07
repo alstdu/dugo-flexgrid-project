bf29655d5d924986b7332884e06f7ec5cd073217
alstdu <lexi.dugo@gmail.com>
2024-03-07 12:56:45 -0800

    oops that was this commit

---
+++ scripts/scripts.js
@@ -0,0 +1,61 @@
+const sliderImages = [
+    'rocket', 'space', 'telescope',
+];
+
+const dataBuckets = sliderImages.map( ( sliderImage ) => ( {
+    sliderImage,
+    fullImage: `./images/${sliderImage}-full.jpg`,
+    thumbElement: document.createElement( 'img' ),
+} ) );
+
+let currentIndex = null;
+const lastIndex = sliderImages.length - 1;
+
+const firstLoad = () => {
+    setNav( 0 );
+    document.querySelector( '#prev-btn' ).addEventListener( 'click', setNavPrev );
+    document.querySelector( '#next-btn' ).addEventListener( 'click', setNavNext );
+};
+
+const setMainImage = ( index ) => {
+    document.querySelector( '#main-image' ).src = dataBuckets[index].fullImage;
+    console.log( dataBuckets[index] );
+};
+
+const setNav = ( index ) => {
+    currentIndex = index;
+    setMainImage( currentIndex );
+};
+
+const setNavNext = () => {
+    const nextIndex = ( currentIndex == lastIndex ) ? 0 : currentIndex + 1;
+    setNav( nextIndex );
+    stopAutoRotation();
+};
+
+const setNavPrev = () => {
+    const prevIndex = ( currentIndex == 0 ) ? lastIndex : currentIndex - 1;
+    setNav( prevIndex );
+    stopAutoRotation();
+};
+
+const rotateInterval = 7000;
+let autoRotateTimer;
+
+const startAutoRotation = () => {
+    autoRotateTimer = setInterval( () => {
+        setNavNext();
+    }, rotateInterval );
+};
+
+const stopAutoRotation = () => {
+    clearInterval( autoRotateTimer );
+    startAutoRotation();
+};
+
+window.addEventListener( 'load', () => {
+    firstLoad();
+    startAutoRotation();
+} );
+
+// Lexi Dugo A01348881 2024
