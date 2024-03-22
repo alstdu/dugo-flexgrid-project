805b080f2ab3fd71ec5187ce862ff20807a20d57
alstdu <lexi.dugo@gmail.com>
2024-03-21 19:30:45 -0700

    Remove previous custom slider and import glide.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,5 @@
+import Glide, {Autoplay, Controls} from '../node_modules/@glidejs/glide/dist/glide.modular.esm.js';
+
 const sliderImages = [
     'rocket', 'space', 'telescope',
 ];
@@ -7,56 +9,6 @@ const dataBuckets = sliderImages.map( ( sliderImage ) => ( {
     fullImage: `./images/${sliderImage}-full.jpg`,
     thumbElement: document.createElement( 'img' ),
 } ) );
-
-let currentIndex = null;
-const lastIndex = sliderImages.length - 1;
-
-const firstLoad = () => {
-    setNav( 0 );
-    document.querySelector( '#prev-btn' ).addEventListener( 'click', setNavPrev );
-    document.querySelector( '#next-btn' ).addEventListener( 'click', setNavNext );
-};
-
-const setMainImage = ( index ) => {
-    document.querySelector( '#main-image' ).src = dataBuckets[index].fullImage;
-    console.log( dataBuckets[index] );
-};
-
-const setNav = ( index ) => {
-    currentIndex = index;
-    setMainImage( currentIndex );
-};
-
-const setNavNext = () => {
-    const nextIndex = ( currentIndex == lastIndex ) ? 0 : currentIndex + 1;
-    setNav( nextIndex );
-    stopAutoRotation();
-};
-
-const setNavPrev = () => {
-    const prevIndex = ( currentIndex == 0 ) ? lastIndex : currentIndex - 1;
-    setNav( prevIndex );
-    stopAutoRotation();
-};
-
-const rotateInterval = 7000;
-let autoRotateTimer;
-
-const startAutoRotation = () => {
-    autoRotateTimer = setInterval( () => {
-        setNavNext();
-    }, rotateInterval );
-};
-
-const stopAutoRotation = () => {
-    clearInterval( autoRotateTimer );
-    startAutoRotation();
-};
-
-window.addEventListener( 'load', () => {
-    firstLoad();
-    startAutoRotation();
-} );
 
 const hamburgerMenu = document.querySelector( '.menu-icon' );
 hamburgerMenu.addEventListener( 'click', () => {
@@ -71,5 +23,12 @@ const closeNav = () => {
 
 document.querySelector( '#closeMenuBtn' ).addEventListener( 'click', closeNav );
 document.querySelector( '.nav-open-overlay' ).addEventListener( 'click', closeNav );
+
+new Glide( '.glide', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 1,
+    focusAt: 'center',
+} ).mount( {Autoplay, Controls} );
 
 // Lexi Dugo A01348881 2024
